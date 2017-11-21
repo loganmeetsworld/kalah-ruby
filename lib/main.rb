d708e3e973b393b12309f47e6340c699ff721ea0
Logan McDonald <loganmcdona11@gmail.com>
2017-11-21 11:05:40 -0800

    basic board logic working

--- lib/main.rb
+++ lib/main.rb
@@ -1,47 +1,43 @@
 INTRO_SCRIPT = """
-Welcome to Kalah. Please select a puddle on the board 1-5.
+Welcome to Kalah. Please select a puddle on the board 1-6.
 """
 
-class Board
+class Game
   attr_accessor :board, :presenter_board
 
   def initialize
-    @board = [4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 0]
+    @board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
   end
   
   def print_board
-    puts "them: [#{@board[11]}] #{@board[6..10].reverse.to_s}"
-    puts "you:      #{@board[0..4].to_s} [#{@board[5]}]"
+    # puts @board.to_s
+    puts "them: [#{@board[13]}] #{@board[7..12].reverse.to_s}"
+    puts "you:      #{@board[0..5].to_s} [#{@board[6]}]"
   end
 
-  def move_pieces_right(place)
-    pebbles = @board[place - 1]
-    @board[place - 1] = 0
-    pebbles.times do |x|
-      @board[place] += 1
-      place += 1
-    end
-  end
-
-  def move_pieces_left
-    place = rand(6..10)
-    puts place
+  def move_pieces(place, player)
     pebbles = @board[place]
     @board[place] = 0
-    pebbles.times do |x|
-      @board[place + 1] += 1
-      place += 1
+    place = place == 13 ? 0 : place + 1
+    while pebbles > 0
+      if player == '2' && place == 6 then place = place == 13 ? 0 : place + 1; next end
+      if player == '1' && place == 13 then place = place == 13 ? 0 : place + 1; next end
+      pebbles -= 1
+      @board[place] += 1
+      place = place == 13 ? 0 : place + 1
     end
-
   end
 end
 
 puts INTRO_SCRIPT
-b = Board.new
+g = Game.new
+puts "starting board: #{g.print_board}"
 loop do
   place = gets.chomp
-  b.move_pieces_right(place.to_i)
-  puts b.print_board
-  b.move_pieces_left
-  puts b.print_board
+  g.move_pieces(place.to_i - 1, "1")
+  puts g.print_board
+  other_place = rand(7..12)
+  puts other_place - 6
+  g.move_pieces(other_place, "2")
+  puts g.print_board
 end
