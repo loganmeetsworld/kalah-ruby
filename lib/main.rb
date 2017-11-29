9c45f571edcab16c1886d5ec6d92a828c1a28cbc
Logan McDonald <loganmcdona11@gmail.com>
2017-11-29 16:19:02 -0500

    some reorganizing

--- lib/main.rb
+++ lib/main.rb
@@ -1,98 +1,128 @@
-INTRO_SCRIPT = """
-Welcome to Kalah. Please select a puddle on the board 1-6 using the numbers on your keyboard.
-"""
-
 class Player
-  attr_accessor :type
-  def initialize(type)
-    @type = type
+  attr_accessor :player_type
+  def initialize(player_type)
+    @player_type = player_type
   end
   
   def human?
-    @type == 'human'
+    @player_type == 'human'
   end
 
-  def robot?
-    @type == 'robot'
+  def ai?
+    @player_type == 'ai'
   end
 end
 
-class Game
+class Board
   attr_accessor :board, :presenter_board
 
   def initialize(p1, p2)
-    @board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
+    @puddles = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
     @p1 = p1
     @p2 = p2
   end
   
   def print_board
-    puts "them: [#{@board[13]}] #{@board[7..12].reverse.to_s}"
-    puts "you:      #{@board[0..5].to_s} [#{@board[6]}]"
+    puts "them: [#{@puddles[13]}] #{@puddles[7..12].reverse.to_s}"
+    puts "you:      #{@puddles[0..5].to_s} [#{@puddles[6]}]"
   end
 
   def game_over?
-    @board.slice(0, 6).inject(&:+) == 0 || @board.slice(7, 14).inject(&:+) == 0
+    @puddles.slice(0, 6).inject(&:+) == 0 || @puddles.slice(7, 14).inject(&:+) == 0
   end
 
-  def empty_pit?
+  def empty_pit?(pit)
+    @puddles[pit] == 0
+  end
+
+  def opposite_pit_full?(pit)
+    pit + 6 != 0
   end
 
   def check_for_empty_pit
   end
 
   def who_won?
-    if @board[7] == @board[-1]
+    if @puddles[7] == @puddles[-1]
       puts "TIE!"
-    elsif @board[7] > @board[-1]
+    elsif @puddles[7] > @puddles[-1]
       puts "YOU WON!"
     else
       puts "YOU LOST!"
     end
   end
 
+  def exit?
+    gets.chomp == 'quit' || gets.chomp == 'quit'
+  end
+
   def move_pieces(player)
-    place = player.human? ? gets.chomp.to_i - 1 : rand(7..12)
-    pebbles = @board[place]
-    if pebbles == 0 || !place.is_a?(Numeric)
-      puts "try again"
+    pit = player.human? ? gets.chomp.to_i - 1 : rand(7..12)
+    pebbles = @puddles[pit]
+    if pebbles == 0 || !pit.is_a?(Numeric)
+      if player.human? then puts "Invalid. Try again." end
       move_pieces(player)
     end
-    @board[place] = 0
-    place += 1
-    place = 0 if place == 13
+    # if empty_pit?(pit) && opposite_pit_full?(pit)
+    #   @puddles[6] += (@puddles[pit] + @puddles[pit + 6])
+    # else
+
+    @puddles[pit] = 0
+    pit += 1
+    pit = 0 if pit == 13
     while pebbles > 0
-      if player.robot? && place == 6 then place = place == 13 ? 0 : place + 1; next end
-      if player.human? && place == 13 then place = place == 13 ? 0 : place + 1; next end
+      if player.ai? && pit == 6 then pit = pit == 13 ? 0 : pit + 1; next end
+      if player.human? && pit == 13 then pit = pit == 13 ? 0 : pit + 1; next end
       pebbles -= 1
-      @board[place] += 1
-      place = place == 13 ? 0 : place + 1
+      @puddles[pit] += 1
+      pit = pit == 13 ? 0 : pit + 1
     end
-    puts "place: #{place}, player: #{player}"
-    if player.human? && place == 7 || player.robot? && place == 13
+
+    if player.human? && pit == 7 || player.ai? && pit == 13
       print_board
-      puts "#{player} gets to go again!"
+      puts "#{player.player_type} gets to go again!"
       move_pieces(player)
     end
   end
 end
 
-puts INTRO_SCRIPT
-human_player = Player.new('human')
-robot_player = Player.new('robot')
-g = Game.new(human_player, robot_player)
-puts "starting board: #{g.print_board}"
-loop do
-  if g.game_over?
-    g.who_won?
-    break
+class Game
+  attr_accessor :board
+
+  def initialize(player_1, player_2)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = Board.new(player_1, player_2)
   end
-  g.move_pieces(human_player)
-  puts g.print_board
-  if g.game_over?
-    g.who_won?
-    break
+
+  def print_instructions
+    intro_script = """
+    Welcome to Kalah. Please select a puddle on the board 1-6 using the numbers on your keyboard.
+    """
+    puts intro_script
+    puts @board.print_board
   end
-  g.move_pieces(robot_player)
-  puts g.print_board
+
+  def start_game
+    loop do
+      if @board.game_over?
+        @board.who_won?
+        break
+      end
+      @board.move_pieces(@player_1)
+      puts @board.print_board
+      if @board.game_over?
+        @board.who_won?
+        break
+      end
+      @board.move_pieces(@player_2)
+      puts @board.print_board
+    end
+  end
 end
+
+human_player = Player.new('human')
+ai_player = Player.new('ai')
+game = Game.new(human_player, ai_player)
+game.print_instructions
+game.start_game
